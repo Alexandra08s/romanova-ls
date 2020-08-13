b51e0c5f5745322fece56765451c8be9420ff9a1
Alexandra08s <alexromanova8s@gmail.com>
2020-08-13 14:47:37 +0300

    reviews desktop, tablet, phone & skills vue widget

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -1,3 +1,21 @@
-// new Vue({
-//   el: #
-// })
+import Vue from "vue";
+// import { component } from "vue/types/umd";
+
+const skillsProgress = {
+  template: '#skills-progress'
+}
+
+const skillsChart = {
+  template: '#skills-chart',
+  components: {
+    skillsProgress
+  }
+}
+
+new Vue({
+  el: '#skills-component',
+  template: '#skills-block',
+  components: {
+    skillsChart
+  }
+})
