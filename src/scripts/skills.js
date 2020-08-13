f788b8206ff39d7d43bf573994135cd70a1c7464
Alexandra08s <alexromanova8s@gmail.com>
2020-08-13 18:45:35 +0300

    skills vue widget

--- src/scripts/skills.js
+++ src/scripts/skills.js
@@ -2,20 +2,51 @@ import Vue from "vue";
 // import { component } from "vue/types/umd";
 
 const skillsProgress = {
-  template: '#skills-progress'
+  props: { 
+    skill: {
+      type: Object,
+      required: true
+    }
+  },
+  mounted() {
+    const circle = this.$refs["circle-progress"]
+    const dashOffset = getComputedStyle(circle).getPropertyValue("stroke-dashoffset").replace(/px/, '')
+    const newDashOffset = dashOffset - dashOffset*this.skill.percent/100
+
+    circle.style.dashOffset = newDashOffset
+    debugger
+  },
+  template: '#skills-progress',
 }
 
 const skillsChart = {
-  template: '#skills-chart',
   components: {
     skillsProgress
-  }
+  },
+  props: {
+    category: {
+      type: Object,
+      required: true
+    }
+  },
+  template: '#skills-chart'
 }
 
 new Vue({
   el: '#skills-component',
-  template: '#skills-block',
   components: {
     skillsChart
-  }
+  },
+  data() {
+    return {
+      skills: []
+    }
+  },
+  created() {
+    this.skills = require('../data/skills.json')
+  },
+  mounted() {
+
+  },
+  template: '#skills-block'
 })
