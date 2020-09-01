09b9d2849165fda18d33129563daf363a7aab45b
Alexandra08s <alexromanova8s@gmail.com>
2020-09-01 21:25:44 +0300

    reviews page

--- src/admin/components/pages/reviews/reviews-page.vue
+++ src/admin/components/pages/reviews/reviews-page.vue
@@ -4,6 +4,22 @@
       <p class="page__title">
         Блок «Отзывы»
       </p>
+      <ul class="reviews__list">
+        <li v-if="emptyReviews" class="reviews__item">
+          <review
+            empty
+          />
+        </li>
+        <li
+          v-for="review in reviews"
+          :key="review.id"
+          class="reviews__item"
+        >
+          <review
+            :review="review"
+          />
+        </li>
+      </ul>
     </div>
   </section>
 </template>
