322f3e964724c08ff89f9e3830bfce9b46345305
Alexandra08s <alexromanova8s@gmail.com>
2020-09-18 12:34:27 +0300

    week5 router fixes

--- src/admin/router.js
+++ src/admin/router.js
@@ -51,31 +51,35 @@ const guard = axios.create({
   baseURL: 'https://webdev-api.loftschool.com/'
 })
 
-guard.interceptors.response.use(response => response, 
-  async function (error) {
-    if (error.response.status === 401) {
-      const originalRequest = error.config
-      try {
-        axios.defaults.baseURL = 'https://webdev-api.loftschool.com/'
-        axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
-        const response = await axios.post('/refreshToken')
-        localStorage.setItem('token', response.data.token)
-        originalRequest.headers['Authorization'] = `Bearer ${response.data.token}`
-      } catch(error) {
-        console.log(error)
-      }
+const tokenString = localStorage.getItem('token')
 
-      return guard(originalRequest)
-    }
-    return Promise.reject(error)
-  })
+if (tokenString) {
+  guard.interceptors.response.use(response => response, 
+    async function (error) {
+      if (error.response.status === 401) {
+        const originalRequest = error.config
+        try {
+          axios.defaults.baseURL = 'https://webdev-api.loftschool.com/'
+          axios.defaults.headers.common['Authorization'] = `Bearer ${localStorage.getItem('token')}`
+          const response = await axios.post('/refreshToken')
+          localStorage.setItem('token', response.data.token)
+          originalRequest.headers['Authorization'] = `Bearer ${response.data.token}`
+        } catch(error) {
+          console.log(error)
+        }
+  
+        return guard(originalRequest)
+      }
+      return Promise.reject(error)
+    })
+}
 
 router.beforeEach(async (to, from, next) => {
   const isPublicRoute = to.matched.some(route => route.meta.public)
   const isUserLoggedIn = store.getters['user/userIsloggedIn']
 
   if (!isPublicRoute && !isUserLoggedIn) {
-    const token = localStorage.getItem('token', token)
+    const token = localStorage.getItem('token')
     guard.defaults.headers['Authorization'] = `Bearer ${token}`
     try {
       const response = await guard.get('/user')
